778aa74f1d8e2c41f9d5f586286ac95ccc74d708
lammiejay <lammiejay02@gmail.com>
2023-08-08 03:10:55 +0100

    joined Babel and Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,17 @@ module.exports = {
         },
         port: 3000,
         hot: "only"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
     }
 };
