819a539b1696f36fe6927664df5868bc3a0cf166
lammiejay <lammiejay02@gmail.com>
2023-08-08 02:50:59 +0100

    added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist/assets'),
         filename: 'bundle.js'
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        devMiddleware: {
+        publicPath: '/assets/'
+        },
+        port: 3000,
+        hot: "only"
     }
 };
